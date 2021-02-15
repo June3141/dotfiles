49455caa0b6b27057d38c7177d96a2a66f1c271e
june3141 <ryoushi0620@gmail.com>
2021-02-15 20:49:34 +0900

    add python env

--- zsh/config/lang.zsh
+++ zsh/config/lang.zsh
@@ -11,9 +11,25 @@ export GNUPGHOME="${ASDF_DIR}/keyrings/nodejs"
 export PATH="${HOME}/.npm-global/bin:$PATH"
 export NPM_CONFIG_PREFIX="${HOME}/.npm-global"
 
+# ==== JavaScript ============================================================
+# deno
+export PATH="${HOME}/.deno/bin:${PATH}"
+
 # ==== python ================================================================
 # poetry
 export PATH="$HOME/.poetry/bin:$PATH"
+
+# pyenv
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init -)"
+
+# pipenv
+export PIPENV_VENV_IN_PROJECT=true
+
+# conda
+# https://poyo.hatenablog.jp/entry/2019/03/19/133350#Conda
+alias activate_conda_env='source $(conda info --root)/etc/profile.d/conda.sh'
 
 # ==== rust ==================================================================
 # rustup env
@@ -26,4 +42,3 @@ export PATH="${HOME}/.gem/ruby/2.7.0/bin:${PATH}"
 # ==== terraform =============================================================
 # tfenv (virtual env for terraform)
 export PATH="${HOME}/.tfenv/bin:${PATH}"
-
